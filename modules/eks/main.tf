a510c2dcc3021da58baf2f8ab78d46666b171cef
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-01-30 16:50:45 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -3,26 +3,34 @@ data "aws_vpc" "default" {
   default = true
 }
 
+# Fetch subnets of the default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
 
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn  = aws_iam_role.eks.arn
   version   = "1.31"
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
 }
 
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.eks.arn
-  subnet_ids = [
-      us-east-1a, us-east-1b, us-east-1c, us-east-1d
-    ]
+  subnet_ids      = data.aws_subnets.default.ids
   scaling_config {
     desired_size = 2
-    max_size     = 2
+    max_size     = 3
     min_size     = 1
   }
   instance_types = var.instance_types
 }
-
 

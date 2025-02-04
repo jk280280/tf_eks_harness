22eee058dca1ecfe1907d50911fcbb058d3f2fd7
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-02-04 16:05:28 +0530

    Update iam.tf

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -29,4 +29,9 @@ resource "aws_iam_role_policy_attachment" "ec2" {
   role       = aws_iam_role.eks.name
   policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+# Attach the necessary VPC CNI permissions to the EKS worker node role
+resource "aws_iam_role_policy_attachment" "vpc_cni" {
+  role       = aws_iam_role.eks.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"  # Required for VPC CNI plugin
+}
 

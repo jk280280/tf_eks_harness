4989d04da734d01a03967a7c3fd37d5c6ca398b5
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-02-04 17:19:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
 
 module "eks" {
   source          = "./modules/eks"
@@ -15,19 +10,5 @@ module "eks" {
   instance_types  = ["t3.medium"]
 }
 
-module "delegate" {
-  source = "harness/harness-delegate/kubernetes"
-  version = "0.1.8"
-
-  account_id = "axO8S93qRGqqf1tlBaonnQ"
-  delegate_token = "OWYyNDYzMjVlODVkZTJlY2RiZmFlZjM2NmEzMDk3N2Y="
-  delegate_name = "terraform-delegate"
-  deploy_mode = "KUBERNETES"
-  namespace = "harness-delegate-ng"
-  manager_endpoint = "https://app.harness.io"
-  delegate_image = "harness/delegate:25.01.85000"
-  replicas = 1
-  upgrader_enabled = true
-}
 
 

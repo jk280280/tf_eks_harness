4f5306359c23bdbdf1da3afff50d7413a9ce7600
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-02-28 14:23:01 +0530

    Update eks-harness-delegate.tf

--- eks-harness-delegate.tf
+++ eks-harness-delegate.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "eks_cluster" {
-  name = aws_eks_cluster.eks_cluster.name
+  name = aws_eks_cluster.this.name
 }
 
 data "aws_eks_cluster_auth" "eks_auth" {
-  name = aws_eks_cluster.eks_cluster.name
+  name = aws_eks_cluster.this.name
 }
 
 provider "kubernetes" {

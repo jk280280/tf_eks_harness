a31588e66748066f058d2b8b2b1a96f03492ca24
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-02-28 16:14:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ provider "aws" {
   region = "us-west-1"
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
 module "eks" {
   source          = "./modules/eks"
   cluster_name    = "my-eks-cluster"
@@ -15,20 +9,49 @@ module "eks" {
   instance_types  = ["t3.medium"]
 }
 
-module "delegate" {
-  source = "harness/harness-delegate/kubernetes"
-  version = "0.1.8"
-
-  account_id = "axO8S93qRGqqf1tlBaonnQ"
-  delegate_token = "OWYyNDYzMjVlODVkZTJlY2RiZmFlZjM2NmEzMDk3N2Y="
-  delegate_name = "terraform-delegate"
-  deploy_mode = "KUBERNETES"
-  namespace = "harness-delegate-ng"
-  manager_endpoint = "https://app.harness.io"
-  delegate_image = "harness/delegate:25.02.85300"
-  replicas = 1
-  upgrader_enabled = true
+# Fetch EKS cluster details dynamically
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_name
 }
 
+data "aws_eks_cluster_auth" "eks_auth" {
+  name = module.eks.cluster_name
+}
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  token                  = data.aws_eks_cluster_auth.eks_auth.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+}
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    token                  = data.aws_eks_cluster_auth.eks_auth.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  }
+}
+
+# Create Harness Delegate Namespace
+resource "kubernetes_namespace" "harness_delegate" {
+  metadata {
+    name = "harness-delegate-ng"
+  }
+}
+
+module "delegate" {
+  source          = "harness/harness-delegate/kubernetes"
+  version         = "0.1.8"
+
+  account_id      = "axO8S93qRGqqf1tlBaonnQ"
+  delegate_token  = "OWYyNDYzMjVlODVkZTJlY2RiZmFlZjM2NmEzMDk3N2Y="
+  delegate_name   = "terraform-delegate"
+  deploy_mode     = "KUBERNETES"
+  namespace       = kubernetes_namespace.harness_delegate.metadata[0].name
+  manager_endpoint = "https://app.harness.io"
+  delegate_image  = "harness/delegate:25.02.85300"
+  replicas        = 1
+  upgrader_enabled = true
+
+  depends_on = [module.eks, kubernetes_namespace.harness_delegate]
+}

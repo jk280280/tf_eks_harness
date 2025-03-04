e504bd0f7bdec8169a6bcf4c54146b971cf24d89
jk280280 <38111277+jk280280@users.noreply.github.com>
2025-03-04 12:27:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,10 +12,12 @@ module "eks" {
 # Fetch EKS cluster details dynamically
 data "aws_eks_cluster" "eks" {
   name = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "eks_auth" {
   name = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 provider "kubernetes" {
